510b83afd45f85e4650ab717b778fa55b62b3a60
MNERONE <66094699+MNERONE@users.noreply.github.com>
2022-04-08 11:43:19 -0300

    time picker spinner

--- prd_cloud_app/lib/widgets/date_time_picker.dart
+++ prd_cloud_app/lib/widgets/date_time_picker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
 import 'package:intl/intl.dart';
 
 typedef DateTimePickerSetter = void Function(DateTime? newValue);
@@ -34,7 +35,9 @@ class DateTimePicker extends StatelessWidget {
     );
     if (pickedDate == null) return;
 
-    final TimeOfDay? pickedTime = await showTimePicker(context: context, initialTime: TimeOfDay(hour: _date?.hour ?? 0, minute: _date?.minute ?? 0));
+    final TimeOfDay? pickedTime = await showTimePickerSpin(context: context, initialTime: TimeOfDay(hour: _date?.hour ?? 0, minute: _date?.minute ?? 0));
+
+    
     if (pickedTime == null) return;
 
     var pickedDateTime = DateTime(pickedDate.year, pickedDate.month, pickedDate.day, pickedTime.hour, pickedTime.minute, 0);
@@ -65,5 +68,80 @@ class DateTimePicker extends StatelessWidget {
         ),
       ),
     );
+  }
+
+Future<TimeOfDay?> showTimePickerSpin({ required BuildContext context, TimeOfDay? initialTime }) async {
+  return showDialog<TimeOfDay>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return TimpePickerSpin(initialTime: initialTime);
+    },
+  );
+}
+
+
+
+}
+
+class TimpePickerSpin extends StatefulWidget {
+  TimpePickerSpin({ 
+    Key? key, 
+    this.initialTime,
+  }) : super(key: key);
+
+  TimeOfDay? initialTime;
+
+  @override
+  State<TimpePickerSpin> createState() => _TimpePickerSpinState();
+}
+
+class _TimpePickerSpinState extends State<TimpePickerSpin> {
+
+  TimeOfDay? time;
+
+  @override
+  void initState() {
+    time = widget.initialTime;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+        title: Text('SELECIONAR HORÁRIO', style: Theme.of(context).textTheme.titleSmall),
+        content: TimePickerSpinner(
+          is24HourMode: true,
+          normalTextStyle: const TextStyle(
+
+          ),
+          highlightedTextStyle: const TextStyle(
+          ),
+          spacing: 30,
+          itemHeight: 30,
+          time: DateTime(2020, 1, 1, widget.initialTime?.hour ?? 0, widget.initialTime?.minute ?? 0),
+          isForce2Digits: true,
+          
+          onTimeChange: (time) {
+            setState(() {
+              this.time = TimeOfDay(hour: time.hour, minute: time.minute);
+            });
+          },
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('CANCELAR'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop(time);
+            },
+          ),
+        ],
+      );
   }
 }
